a26c3333071e6ae13417f4448efd609b7294e88e
Jimmy Austin <103854385+JCAus@users.noreply.github.com>
2022-06-10 00:58:28 -0400

    Add files via upload

---
+++ Commandline homework.js
@@ -0,0 +1,33 @@
+Part 1:
+
+mkdir first
+cd first
+touch person.txt
+mv person.txt another.txt
+cp another.txt getComputedStyle.txt
+rm getComputedStyle.txt
+cp -r first second
+rm -rf second
+
+Part 2:
+1) The man command pulls up a manual of different commands.  Press enter to scroll and "q" to exit
+
+2) "-l" lists files while the "-a" lists all files included hidden files
+
+3) option left/right
+
+4) ctrl e
+
+5) ctrl a
+
+6)option delete
+
+7)The shell is handling commands that are given by the user utilizing the terminal as an interface
+
+8)an absolute path starts at the root and gives the complete location of a file by listing each folder
+
+9)a relative path does not start at the root but at the current location
+
+10)users add flags to gain more functionality out of a command.  some flags include "-a", "-l", and "-r"
+
+11)"-r" removes files recursively while "-f" will force remove files
