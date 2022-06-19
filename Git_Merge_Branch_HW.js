f841997a8215e60a0c9f2e04472c47c0cd5e2956
Jimmy Austin <103854385+JCAus@users.noreply.github.com>
2022-06-18 20:44:15 -0400

    Add files via upload

---
+++ Git_Merge_Branch_HW.js
@@ -0,0 +1,5 @@
+// 1) git checkout -b
+// 2) fast forwards can only happen if there have not been commits on the original branch while the new branch is being worked on
+// 3) git checkout
+// 4) git branch -D
+// 5) merge conflicts happen when git cannot determine which file to choose from since the same file has different commits with changes
