deb314c683421c039ade67384c37876f2198acd0
Jimmy Austin <103854385+JCAus@users.noreply.github.com>
2022-07-10 08:45:35 -0400

    Add files via upload

---
+++ ES2015_HW.js
@@ -0,0 +1,11 @@
+const Pi = 3.14;
+Pi = 42;
+
+//What is the difference between var and let?
+"Var allows for reassignment and redeclaration, while let only allows for reassignment.  You can access a hoisted variable with var and let creates block scope"
+//What is the difference between var and const?
+"Var allows for reassignment and redeclaration, and you can't do either with const.  You can access a hoisted variable with var and const creates block scope"
+//What is the difference between let and const?
+"You can't redeclare with let or const, but with let you can still reassign and with const you cannot"
+//What is hoisting?
+"When we use 'var', the variable declaration is ran as if it was at the top, as if its been initialized.  Until given a value, var variable will print undefined if logged."
