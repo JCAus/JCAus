b09551a134fe51b845b3ad397fe6c2ca12d120f2
Jimmy Austin <103854385+JCAus@users.noreply.github.com>
2022-07-30 19:32:15 -0400

    Add files via upload

---
+++ HowTheWebWorksHomework.js
@@ -0,0 +1,11 @@
+//Solidify Terminology
+// 1. http stands for Hypertext Transfer Protocol. Protocols are the conventions and ways of how one thing talks to another
+// 2. URL stands for uniform resource locator. It makes it possible for a computer to open a web page located on a different computer on the Internet
+// 3. DNS stands for Domain Name System. It's purpose is to take a host name, like facebook.com, and translate it to the coresponding IP address
+// 4. A query string is a part of a uniform resource locator (URL) that assigns values to specified parameters. 
+// A query string commonly includes fields added to a base URL by a Web browser or other client application, for example as part of an HTML, choosing the appearance of a page, or jumping to positions in multimedia content.
+// 5. GET: gets data from server.  POST: sends data to server.
+// 6. An HTTP request is a request from a client to a server which follows the HTTP protocol
+// 7. An HTTP response is a response from a server to a client which follows the HTTP protocol
+// 8. Header give additional info on a request or response. Some request headers are: Host, Accept, Cookie. Some response headers are: Content-type, Last-modified, Set-cookie.
+// 9. Browser uses DNS to convert name into an IP address.  Then the browser makes a request to said IP address.  The server will then send a response. Then the browser makes a DOM from that HTML, finding any other resources needed.  Then the browser makes separate HTTP requests for those resources and receives response from the server for each.
